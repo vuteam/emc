2fc28bd5f25974cdf6173a00241bd8af35571f90
vuteam <rakzami39@gmail.com>
2019-10-12 13:41:12 +0100

    Create eframe.h

---
+++ lib/gui/eframe.h
@@ -0,0 +1,20 @@
+#ifndef __lib_gui_eframe_h
+#define __lib_gui_eframe_h
+
+#include <lib/gui/elabel.h>
+#include <lib/python/connections.h>
+
+class eFrame: public eLabel
+{
+public:
+	eFrame(eWidget *parent);
+	PSignal0<void> selected;
+
+	void push();
+  void setFrameSize();
+  void setFrameOffset();
+protected:
+	int event(int event, void *data=0, void *data2=0);
+};
+
+#endif
