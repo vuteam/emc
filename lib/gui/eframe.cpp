e1936b28c32b5a6bb4812d1a84cb7bcebb0f55dc
vuteam <rakzami39@gmail.com>
2019-10-12 18:08:20 +0100

    Update eframe.cpp

--- lib/gui/eframe.cpp
+++ lib/gui/eframe.cpp
@@ -39,51 +39,9 @@ void eFrame::setFrameSize()
 	selected();
 }
 
-int eFrame::event(int event, void *data, void *data2)
-{
-	switch (event)
-	{
-	case evtPaint:
-	{
-		gPainter &painter = *(gPainter*)data2;
-		ePtr<eWindowStyle> style;
-
-		getStyle(style);
-
-		eLabel::event(event, data, data2);
-		style->drawFrame(painter, eRect(ePoint(0, 0), size()), eWindowStyle::frameFrame);
-
-		return 0;
-	}
-	default:
-		break;
-	}
-	return eLabel::event(event, data, data2);
-}
 
 void eFrame::setFrameOffset()
 {
 	selected();
 }
 
-int eFrame::event(int event, void *data, void *data2)
-{
-	switch (event)
-	{
-	case evtPaint:
-	{
-		gPainter &painter = *(gPainter*)data2;
-		ePtr<eWindowStyle> style;
-
-		getStyle(style);
-
-		eLabel::event(event, data, data2);
-		style->drawFrame(painter, eRect(ePoint(0, 0), size()), eWindowStyle::frameFrame);
-
-		return 0;
-	}
-	default:
-		break;
-	}
-	return eLabel::event(event, data, data2);
-}
